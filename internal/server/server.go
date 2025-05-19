c60b04f4317087110ad648ed0b0cdc6eb6853be3
walther_knight <knightofargh@gmail.com>
2025-05-19 14:39:20 -0400

    Update to use /app and create health endpoint

--- internal/server/server.go
+++ internal/server/server.go
@@ -12,8 +12,15 @@ func Start() error {
 		Addr:    ":8080",
 	}
 	log.Println("Starting handlers...")
-	newMux.Handle("/", http.FileServer(http.Dir("./static")))
+	newMux.HandleFunc("/healthz", health)
+	newMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./static"))))
 
 	log.Printf("Starting http server on %s\n", httpSrv.Addr)
 	return httpSrv.ListenAndServe()
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
