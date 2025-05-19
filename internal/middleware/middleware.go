bf2da8cdbd1eafe5dbd8bf0ff8ee8935786577bc
walther_knight <knightofargh@gmail.com>
2025-05-19 16:38:56 -0400

    Added log to console for hit and reset endpoints

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"sync/atomic"
 )
@@ -20,13 +21,15 @@ func (cfg *ApiConfig) MiddlewareMetricsInc(next http.Handler) http.Handler {
 
 func (cfg *ApiConfig) HitTotal(w http.ResponseWriter, r *http.Request) {
 	hits := cfg.FileserverHits.Load()
+	log.Printf("HitTotal endpoint hit. Total Hits: %d\n", hits)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf("Hits: %v", hits)))
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
 }
 
 func (cfg *ApiConfig) HitReset(w http.ResponseWriter, r *http.Request) {
 	cfg.FileserverHits.Store(0)
+	log.Println("HitTotal Reset endpoint hit.")
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
